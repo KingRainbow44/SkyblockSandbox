efe3b5e486ac1229dad347da524734a89cad8da3
fusiongamer849 <42220067+fusiongamer849@users.noreply.github.com>
2021-05-22 00:09:10 -0500

    Create StickOfFusion.java

---
+++ src/main/java/tk/skyblocksandbox/skyblocksandbox/item/materials/StickOfFusion.java
@@ -0,0 +1,43 @@
+package tk.skyblocksandbox.skyblocksandbox.item.materials;
+
+import org.bukkit.Material;
+import tk.skyblocksandbox.skyblocksandbox.item.SandboxItem;
+import tk.skyblocksandbox.skyblocksandbox.item.SkyblockItemData;
+import tk.skyblocksandbox.skyblocksandbox.item.SkyblockItemIds;
+import tk.skyblocksandbox.skyblocksandbox.util.Lore;
+
+import java.util.Collection;
+
+public final class StickOfFusion extends SandboxItem {
+
+    public NecronsHandle() {
+        super(Material.STICK, "Stick Of Fusion", SkyblockItemIds.STICK_OF_FUSION);
+    }
+
+    @Override
+    public Collection<String> getLore() {
+        Lore generator = new Lore((16,
+                " ",
+                Utility.colorize("&7Special stick of admin fusion's"),
+                Utility.colorize("&7Edition #1"),
+                Utility.colorize("&7Granted to fusiongamerMC"),
+                " ",)
+        );
+        return generator.genericLore(this);
+    }
+
+    @Override
+    public SkyblockItemData getItemData() {
+        SkyblockItemData itemData = new SkyblockItemData();
+
+        itemData.isMaterial = true;
+        itemData.isDungeonItem = false;
+        itemData.canHaveStars = false;
+        itemData.canReforge = false;
+
+        itemData.rarity = MYTHIC;
+        itemData.itemType = ITEM;
+
+        return itemData;
+    }
+}
